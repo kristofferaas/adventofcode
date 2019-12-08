f0f5352b5309aa1e65e063fbedc76c450d52477d
Kristoffer Aas <mail@kristoffer.io>
2019-12-08 21:17:55 +0100

    Solved day 2 part 2.

--- aoc2019/src/day02.rs
+++ aoc2019/src/day02.rs
@@ -1,10 +1,14 @@
-const INPUT: &str = include_str!("../res/day02.txt");
+pub const INPUT: &str = include_str!("../res/day02.txt");
 
-pub fn part1(input: &str) -> Vec<i32> {
+pub fn part1(input: &str) -> Vec<u32> {
     execute(parse(input))
 }
 
-fn execute(mut tape: Vec<i32>) -> Vec<i32> {
+pub fn part2(input: &str, target: u32) -> u32 {
+    brute_force(parse(input), target)
+}
+
+fn execute(mut tape: Vec<u32>) -> Vec<u32> {
     let mut count: usize = 0;
     loop {
         match tape[count] {
@@ -28,18 +32,33 @@ fn execute(mut tape: Vec<i32>) -> Vec<i32> {
     tape
 }
 
-fn get_values(tape: &Vec<i32>, count: usize) -> (i32, i32) {
+fn get_values(tape: &Vec<u32>, count: usize) -> (u32, u32) {
     let (op1, op2) = (tape[count + 1], tape[count + 2]);
     (tape[op1 as usize], tape[op2 as usize])
 }
 
-fn parse(input: &str) -> Vec<i32> {
+fn parse(input: &str) -> Vec<u32> {
     let opcodes = input
         .trim()
         .split(',')
         .filter_map(|n| n.parse().ok())
-        .collect::<Vec<i32>>();
+        .collect::<Vec<u32>>();
     opcodes
+}
+
+fn brute_force(tape: Vec<u32>, target: u32) -> u32 {
+    for noun in 0..99 {
+        for verb in 0..99 {
+            let mut memory = tape.clone();
+            memory[1] = noun;
+            memory[2] = verb;
+            let result = execute(memory)[0];
+            if target == result {
+                return 100 * noun + verb;
+            }
+        }
+    }
+    0
 }
 
 #[cfg(test)]
@@ -47,7 +66,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn program_example() {
+    fn part1_examples() {
         assert_eq!(
             execute(vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]),
             vec![3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
@@ -62,7 +81,12 @@ mod tests {
     }
 
     #[test]
-    fn part1_with_data() {
+    fn part1_with_given_data() {
         assert_eq!(part1(INPUT)[0], 4945026);
+    }
+
+    #[test]
+    fn part2_with_given_data() {
+        assert_eq!(part2(INPUT, 19690720), 5296);
     }
 }
